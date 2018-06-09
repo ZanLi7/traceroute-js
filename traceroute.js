bfe3d342fef3205ce1ec4c1f3ed2259ba577ac5e
frnkst <frnkst@outlook.com>
2018-06-09 19:03:18 +0200

    resolve dns and check for valid ip address

--- traceroute.js
+++ traceroute.js
@@ -1,10 +1,13 @@
 const dgram = require('dgram');
 const raw = require('raw-socket');
+const dns = require('dns-then');
 
 const icmpSocket = raw.createSocket('icmp');
 const udpSocket = dgram.createSocket('udp4');
 
-const HOST = process.argv[2];
+const DESTINATION_HOST = process.argv[2];
+let DESTINATION_IP;
+
 let PORT;
 let ttl = 0;
 let startTime;
@@ -13,11 +16,32 @@ let timeout;
 const MAX_TIMEOUT_IN_MILLISECONDS = 5000;
 const MAX_HOPS = 64;
 
-console.log(`traceroute to ${HOST} (${HOST}), ${MAX_HOPS} hops max, 42 byte packets`);
-
-udpSocket.bind(1234, () => {
-  sendPacket();
+setImmediate(() => {
+  icmpSocket.on('message', function (buffer, source) {
+    let p = buffer.toString('hex').substr(100, 4);
+    let portNumber = parseInt(p, 16);
+    if (PORT === portNumber) {
+      logReply(source);
+    }
+  });
 });
+
+function getIPAddress(host) {
+  const validIPAddress = new RegExp("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+  if (!validIPAddress.test(host)) return dns.lookup(host);
+  return host;
+}
+
+async function startTrace() {
+  DESTINATION_IP = await getIPAddress(DESTINATION_HOST);
+  console.log(`traceroute to ${DESTINATION_HOST} (${DESTINATION_IP}), 64 hops max, 42 byte packets`);
+
+  udpSocket.bind(1234, () => {
+    sendPacket();
+  });
+}
+
+startTrace();
 
 function sendPacket() {
   startTime = Date.now();
@@ -25,19 +49,13 @@ function sendPacket() {
 
   ttl++;
   udpSocket.setTTL(ttl);
-  udpSocket.send(new Buffer(''), 0, 0, PORT, HOST, function (err) {
+  udpSocket.send(new Buffer(''), 0, 0, PORT, DESTINATION_IP, function (err) {
     if (err) throw err;
     timeout = setTimeout(logReply, MAX_TIMEOUT_IN_MILLISECONDS);
   });
 }
 
-icmpSocket.on('message', function (buffer, source) {
-  let p = buffer.toString('hex').substr(100, 4);
-  let portNumber = parseInt(p, 16);
-  if (PORT === portNumber) {
-    logReply(source);
-  }
-});
+
 
 function logReply(source) {
   if (timeout) {
@@ -48,7 +66,7 @@ function logReply(source) {
     const elapsedTime = Date.now() - startTime;
     console.log(` ${ttl}  ${source} ${elapsedTime} ms`);
 
-    if (source == HOST || ttl >= MAX_HOPS) {
+    if (source == DESTINATION_IP || ttl >= MAX_HOPS) {
       process.exit();
     }
   } else {
